cfa2cbae1dc31efaff61b66ef8353030109297fa
Sandra Masternak <sandra.masternak1@gmail.com>
2025-12-01 23:43:50 +0100

    created styled components for ButtonLink

---
+++ src/features/personalHomepage/PersonalHomepage/ButtonLink/index.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const ButtonLink = styled.a`
+    color: ${({ theme }) => theme.colors.buttonLink.text};
+    background: ${({ theme }) => theme.colors.primary};
+    text-decoration: none;
+    padding: 12px 16px;
+    font-weight: 600;
+    font-size: 20px;
+    border: 1px solid ${({ theme }) => theme.colors.buttonLink.border};
+    border-radius: ${({ theme }) => theme.borderRadiusSmall};
+    transition: box-shadow 0.3s;
+
+    &:hover {
+        box-shadow: 0 0 0 2px ${({ theme }) => theme.colors.buttonLink.shadow}
+    }
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+        font-size: 18px;
+    }
+`;
